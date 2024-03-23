1deaacfe17e005ff89cc52c76c7224f8231350eb
Jiradej Vatanutanon <jiradej.vat@velo.org>
2024-03-23 13:39:57 +0700

    change smart contract name

--- contracts/BTC_Ticket.sol
+++ contracts/BTC_Ticket.sol
@@ -6,11 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+contract BTC_Ticket is ERC20, ERC20Burnable, Ownable, ERC20Permit {
     constructor(address initialOwner)
-        ERC20("MyToken", "MTK")
+        ERC20("BTC_Ticket", "tBTC")
         Ownable(initialOwner)
-        ERC20Permit("MyToken")
+        ERC20Permit("BTC_Ticket")
     {}
 
     function mint(address to, uint256 amount) public onlyOwner {
